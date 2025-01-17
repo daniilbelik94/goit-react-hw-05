150b79fe660fe79fd0887ea134dd6d9b19c2e336
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2025-01-17 09:37:56 +0100

    total

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import ContactForm from './components/ContactForm/ContactForm';
-import SearchBox from './components/SearchBox/SearchBox';
-import ContactList from './components/ContactList/ContactList';
-
-import listcontacts from './listcontacts.json';
-
-import styles from './App.module.css';
+const Navigation = lazy(() => import('./components/Navigation/Navigation'));
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
+const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
 
 function App() {
-  function initialContacts() {
-    const contactsFromLocalStorage = localStorage.getItem('contactsData');
-    return contactsFromLocalStorage ? JSON.parse(contactsFromLocalStorage) : listcontacts;
-  }
-
-  const [contacts, setContacts] = useState(initialContacts());
-  const [searchValue, setSearchValue] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contactsData', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = (newContact) => {
-    setContacts((prevContact) => {
-      return [...prevContact, newContact];
-    });
-  };
-
-  const foundContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(searchValue.toLowerCase()));
-
-  const deletContact = (contactId) => {
-    setContacts((prevContact) => {
-      return prevContact.filter((contact) => contact.id !== contactId);
-    });
-  };
-
-return (
-  <div>
-    <h1 className={styles.title}>Phonebook</h1>
-    <ContactForm onAddContact={addContact} />
-    <SearchBox searchValue={searchValue} onSearch={setSearchValue} />
-    <ContactList contacts={foundContacts} onDelete={deletContact} />
-  </div>
-);
-
+  return (
+    <>
+      <Navigation />
+      <Suspense>
+        <main>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/movies' element={<MoviesPage />} />
+            <Route path='/movies/:movieId' element={<MovieDetailsPage />}>
+              <Route path='cast' element={<MovieCast />} />
+              <Route path='reviews' element={<MovieReviews />} />
+            </Route>
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
